28ac5bedfb0338f6470d165e1dd38079855752d8
KangNansi <larizuri713@gmail.com>
2019-04-05 01:00:09 +0200

    no message

--- lzos.lua
+++ lzos.lua
@@ -1,3 +1,21 @@
 os.loadAPI("lzos/gui")
 
-gui.Button("button")
+local currentEvent = nil
+
+function waitForEvent()
+    while true do
+        currentEvent = os.pullEvent()
+    end
+end
+
+function printEvent()
+    while true do
+        if currentEvent != nil then
+            print(currentEvent)
+            currentEvent = nil
+        end
+    end
+end
+
+parallel.waitForAny(waitForEvent, printEvent)
+
