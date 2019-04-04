7e1d57fb1afd0bd5bc48eaaa8d4dddddd2c64a07
KangNansi <larizuri713@gmail.com>
2019-04-05 01:44:52 +0200

    no message

--- gui.lua
+++ gui.lua
@@ -1,6 +1,20 @@
 
+currentEvent = nil
+ep1, ep2, ep3
 
+function IsIn(x,y, minx, miny, maxx, maxy)
+    return x >= minx and x <= maxx and y >= miny and y <= maxy
+end
 
-function Button(title)
-    paintutils.drawBox(0,0,50,20)
+function PullEvent()
+    currentEvent, ep1, ep2, ep3 = os.pullEvent()
+end
+
+function Button(mon, title, x, y, w, h)
+    mon.setCursorPos(x,y)
+    mon.setTextColor(colors.black)
+    mon.setBackgroundColor(colors.white)
+    mon.write(title)
+    clicked = currentEvent == "monitor_touch" and IsIn(ep2, ep3, x, y, x+w, y+h)
+    return clicked
 end
--- lzos.lua
+++ lzos.lua
@@ -1,31 +1,19 @@
 os.loadAPI("lzos/gui")
 
-local currentEvent = nil
-local ep1, ep2, ep3
+
 local quit = false
 
 args = {...}
 
-monitor = peripheral.wrap(args[1])
+mon = peripheral.wrap(args[1])
 
-function IsIn(x,y, minx, miny, maxx, maxy)
-    return x >= minx and x <= maxx and y >= miny and y <= maxy
-end
 
-function HandleEvent()
-    if currentEvent == "monitor_touch" and ep1 == args[1] then
-        if IsIn(ep2, ep3, 0, 0, 5, 1) then
-            quit = true
-        end
-    end
-end
 
 while not quit do
-    currentEvent, ep1, ep2, ep3 = os.pullEvent()
-    HandleEvent()
-
-    monitor.clear()
-    monitor.setCursorPos(0,0)
-    monitor.write("quit")
+    gui.PullEvent()
+    
+    if gui.Button(mon, "quit", 1, 1, 5, 1) then
+        quit = true
+    end
 end
 
