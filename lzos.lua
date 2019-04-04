8aceff5f3a98fd9b83295da2e042e5027d6ef85d
KangNansi <larizuri713@gmail.com>
2019-04-05 01:27:11 +0200

    no message

--- lzos.lua
+++ lzos.lua
@@ -4,12 +4,16 @@ local currentEvent = nil
 local ep1, ep2, ep3
 local quit = false
 
+args = {...}
+
+monitor = peripheral.wrap(args[1])
+
 function IsIn(x,y, minx, miny, maxx, maxy)
     return x >= minx and x <= maxx and y >= miny and y <= maxy
 end
 
 function HandleEvent()
-    if currentEvent == "mouse_click" and ep1 == 0 then
+    if currentEvent == "monitor_touch" and ep1 == args[1] then
         if IsIn(ep2, ep3, 0, 0, 5, 1) then
             quit = true
         end
@@ -20,8 +24,8 @@ while not quit do
     currentEvent, ep1, ep2, ep3 = os.pullEvent()
     HandleEvent()
 
-    term.clear()
-    term.setCursorPos(0,0)
-    term.write("quit")
+    monitor.clear()
+    monitor.setCursorPos(0,0)
+    monitor.write("quit")
 end
 
