6a5dfaae402bda914d56b7ade58169e7594819ac
KangNansi <larizuri713@gmail.com>
2019-04-05 00:13:16 +0200

    update

--- git.lua
+++ git.lua
@@ -4,15 +4,31 @@ local gaddr = "https://api.github.com/"
 
 local args = {...}
 
+function GetFileName(s)
+    return string.sub(s, 0, string.len(s) - 4)
+end
+
 if args[1] == "get" then
-    local repo = args[2]
+    local dir = args[2]
+    local r = http.get(gaddr.."repos/KangNansi/ComputerCraft/contents/")
+    local s = r.readAll()
 
-    local r = http.get(gaddr..repos/KangNansi/..repo../contents/)
+    local obj = json.decode(s)
 
-    local obj = json.decode(r.readAll())
+    for _, file in pairs(obj) do 
+        print("download..."..file.name)
 
-    for i, v in pairs(obj) do 
-        print(i..": "..v)
-        os.pullEvent()
+        local filename = dir.."/"..GetFileName(file.name)
+        if not fs.isDir(dir) then
+            fs.makeDir(dir)
+        end
+        if fs.exists(filename) then
+            fs.delete(filename)
+        end
+        
+        local fileContent = http.get(file.download_url)
+        local fh = fs.open(filename, "w")
+        fh.write(fileContent.readAll())
+        fh.close()
     end
 end
