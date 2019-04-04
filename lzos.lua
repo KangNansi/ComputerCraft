2436d02a1c8adb359dd885cef4e17a20e29361d6
KangNansi <larizuri713@gmail.com>
2019-04-05 01:13:38 +0200

    no message

--- lzos.lua
+++ lzos.lua
@@ -1,21 +1,27 @@
 os.loadAPI("lzos/gui")
 
 local currentEvent = nil
+local ep1, ep2, ep3
+local quit = false
 
-function waitForEvent()
-    while true do
-        currentEvent = os.pullEvent()
-    end
+function IsIn(x,y, minx, miny, maxx, maxy)
+    return x >= minx and x <= maxx and y >= miny and y <= maxy
 end
 
-function printEvent()
-    while true do
-        if currentEvent != nil then
-            print(currentEvent)
-            currentEvent = nil
+function HandleEvent()
+    if currentEvent == "mouse_click" and ep1 == 0 then
+        if IsIn(ep2, ep3, 0, 0, 5, 1) then
+            quit = true
         end
     end
 end
 
-parallel.waitForAny(waitForEvent, printEvent)
+while not quit do
+    currentEvent, ep1, ep2, ep3 = os.pullEvent()
+    HandleEvent()
+
+    term.clear()
+    term.setCursorPos(0,0)
+    term.write("quit")
+end
 
