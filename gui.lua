34ce40eafc63315cbc7e611eba1434ea6f0eee62
KangNansi <larizuri713@gmail.com>
2019-04-05 02:51:54 +0200

    no message

--- gui.lua
+++ gui.lua
@@ -23,7 +23,7 @@ function Button(mon, title, x, y, w, h)
     mon.setTextColor(colors.black)
     mon.setBackgroundColor(colors.white)
     mon.write(title)
-    clicked = currentEvent == "monitor_touch" and IsIn(ep2, ep3, x, y, x+w-1, y+h-1)
+    clicked = currentEvent == "mouse_click" and IsIn(ep2, ep3, x, y, x+w-1, y+h-1)
     if clicked then repaint = true end
     return clicked
 end
