a9cdf819b190170c737a65089181e7c88144cdd5
KangNansi <evillz@hotmail.com>
2020-06-12 21:13:55 +0800

    Update update.lua

--- update.lua
+++ update.lua
@@ -9,31 +9,59 @@ function GetFileName(s)
 end
 
 local dir = "/lzos"
-local r = http.get(gaddr.."repos/KangNansi/ComputerCraft/contents/")
-local s = r.readAll()
 
-local obj = json.decode(s)
+local dirs = {}
+local dirCount = 0
 
-for _, file in pairs(obj) do 
-    print("download..."..file.name)
-
-    local filename = ""
-
-    if file.name == "startup" then
-        filename = file.name
-    else
-        filename = dir.."/"..GetFileName(file.name)
-    end
-        
-    if not fs.isDir(dir) then
-        fs.makeDir(dir)
-    end
-    if fs.exists(filename) then
-        fs.delete(filename)
-    end
-    
-    local fileContent = http.get(file.download_url)
-    local fh = fs.open(filename, "w")
-    fh.write(fileContent.readAll())
-    fh.close()
+function pushDir(dirName)
+    dirs[dirCount] = dirName
+    dirCount = dirCount + 1
 end
+
+function popDir()
+    dirCount = dirCount - 1
+    if dirCount < 0 then 
+        dirCount = 0 
+    end
+end
+
+function getDir()
+    local r = dir
+    for i = 0, dirCount do
+        r = r.."/"..dirs[i]
+    end
+    return r
+end
+
+function downloadFile(file)
+    print("Downloading "..file.name.."...")
+    local dirPath = getDir()
+    fs.makeDir(dirPath)
+    local filePath = dirPath..GetFileName(file.name)
+    local fileContent = http.get(file.download_url)
+    local file = fs.open(filePath, "w")
+    file.write(fileContent.readAll())
+    file.close()
+end
+
+function downloadGit(target)
+    local addr = gaddr.."repos/KangNansi/ComputerCraft/contents"
+    if target then
+        addr = addr.."/"..target
+    end
+    target = target or ""
+    local content = http.get(addr)
+    local obj = json.decode(content.readAll())
+    for _, file in pairs(obj) do
+        if file.type == "file" then
+            downloadFile(file)
+        elseif file.type == "dir" then
+            pushDir(file.name)
+            downloadGit(target.."/"..file.name)
+            popDir()
+        end
+    end
+end
+
+downloadGit()
+
